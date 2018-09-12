8f1adf8a53a88471ceb25cdc83a572c56bc04a40
sebastien malmberg <43003643+JustaClip@users.noreply.github.com>
2018-09-12 15:54:27 +0200

    Candy
    The method asks user a question.

---
+++ src/something/Mwahaha.java
@@ -0,0 +1,15 @@
+package something;
+import java.util.Scanner;
+
+public class Mwahaha
+{
+   public static void main(String[] args)
+   {
+      Scanner keyboard = new Scanner(System.in);
+      System.out.println("Do you like canady?");
+      String a = keyboard.nextLine();
+      
+    
+   }
+
+}
