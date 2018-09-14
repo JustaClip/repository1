a3960f7533cf374bf2881e9f13eefcfa2921842d
Tamas12345678 <43003631+Tamas12345678@users.noreply.github.com>
2018-09-14 12:17:11 +0100

    New Class
    just a rergular problem

--- haha.java
+++ haha.java
@@ -1,7 +1,20 @@
-
+import java.util.Scanner;
 public class haha {
 	public static void main(String[] args) {
-		
+		Scanner keyboard = new Scanner(System.in);
+		String word;
+		do {
+			System.out.print("Enter a word: ");
+			word = keyboard.nextLine();
+			
+			int number = word.length() - 1;
+			while (n>=0) {
+				System.out.print(word.charAt(number));
+				number--;
+			}
+			System.out.println();
+		}
+		while (!word.equals("quit"));
 	}
 
 }
